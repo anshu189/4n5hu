963f9d1d80d502f0954c4d5046a1029cd6e4c024
anshusaini189381@gmail.com <anshusaini189381@gmail.com>
2024-09-01 23:27:15 +0530

    Added Social Links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 function App() {
   return (
-  <div className="h-screen flex items-center justify-center bg-white">
-    <h1 className="text-8xl text-black font-semibold press-start">4n5hu.</h1>
+  <div className="h-screen flex flex-col items-center justify-center gap-6 bg-white outline-none">
+    <h1 className="text-8xl text-black font-semibold press-start">4n5hu</h1>
+    <div className="flex items-center gap-6 text-2xl lucida">
+      <a href="https://github.com/anshu189">Github</a><span>|</span>
+      <a href="https://www.linkedin.com/in/4n5hu">Linkedin</a><span>|</span>
+      <a href="https://advrtic.com">Advrtic</a>
+    </div>
   </div>
   );
 }
