fbf1ec727f1da52fc77064fc37b8293104859a5e
anshusaini189381@gmail.com <anshusaini189381@gmail.com>
2024-10-27 04:12:51 +0530

    Updated Components Styling

--- src/beams-bg.jsx
+++ src/beams-bg.jsx
@@ -32,7 +32,7 @@ export function BackgroundBeamsDemo() {
           </div>
 
           <Link href="https://drive.google.com/file/d/1YUDYAXXqejsXw2xkyCqBbE1VUxOEAy83/view?usp=sharing">
-            <Button className="mt-4 text-lg" color={color} variant="faded" radius="sm" endContent={<svg className="w-3 rotate-45 fill-[#d59125]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2 160 448c0 17.7 14.3 32 32 32s32-14.3 32-32l0-306.7L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg>}>
+            <Button className="mt-4 text-lg" color={color} variant="faded" radius="sm" endContent={<svg className="w-3 rotate-45 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2 160 448c0 17.7 14.3 32 32 32s32-14.3 32-32l0-306.7L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg>}>
                 Download Resume
             </Button>
           </Link>
